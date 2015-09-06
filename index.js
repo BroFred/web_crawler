03cce8def54909eca741e90a3b6dc9d58a7f5f2b
Brofred <cxy1989112@gmail.com>
2015-09-06 17:30:21 -0400

    auto_refresh web content crawler

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 var express=require('express');
 var request = require('request');
 var app=express();
+var http=require('http').Server(app);
 var cheerio = require('cheerio');
-app.use(express.static('./public'));
+var io=require('socket.io')(http);
+var dic=new Set();
+app.use(express.static('../public'));
+var link='https://en.wikipedia.org/wiki/Nucleic_acid_sequence';
+var calllist=[];
+setInterval(function(){
+	request('http://localhost:3000/start',function(error, response, body){
+			console.log(link);
+	})},5000);
 var mid=function(req,res,next){
-	request('https://en.wikipedia.org/wiki/Nucleic_acid_sequence', function (error, response, body) {
+	request(link, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	  	req.body=body;
 	  	next();
 	  }
 	})
 }
-app.get('/',mid,function(req,res){
+app.get('/start',mid,function(req,res){
 	$ = cheerio.load(req.body);
-	var reg =/[\s\w]+/i
-	console.log(reg.exec($('head').children('title').text())[0]);
-	console.log($('image').attr.toString());
+	var temp=$('.mw-redirect');
+	var t;
+	for( var i in temp ){
+		if(!isNaN(parseInt(i.toString()))){
+			t='https://en.wikipedia.org'+temp[i]['attribs']['href'];
+			if(!dic.has(t)){
+				dic.add(t);
+				calllist.push(t);
+			}
+		}
+	}
+	link=calllist.shift();
+	io.emit('new_page',req.body);
 	res.send(req.body);
 });
-exports.app=app;
+exports.app=http;
